519a23ed0ac9a81f10bdb2509c2c8eb3f52d1ac3
JoSmith40 <jo.smith40@yahoo.com>
2025-09-18 20:18:03 +0200

    Initial Commit

---
+++ Tacho_Mini_Code/Tacho_Mini_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tacho_Mini_CodeApp.swift
+//  Tacho_Mini_Code
+//
+//  Created by Juergen on 18.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Tacho_Mini_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
